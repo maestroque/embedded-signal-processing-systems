ec4d2786e590dc5e50739c7cc6bbedc74a52844c
maestroque <kikasgeorg@ece.auth.gr>
2024-05-28 12:07:23 +0200

    Fix assignment1_4 solution

--- Assignment1/assignment1_4.m
+++ Assignment1/assignment1_4.m
@@ -3,10 +3,15 @@ clear;
 % Load the variables ....
 load("data4.mat")
 
-ones = ones(length(x), 1);
-A = [x' y' ones];
+a1 = cos(x') .* pi / 180;
+a2 = sin(x') .* pi / 180;
+a3 = ones(length(x), 1);
+
+A = [a1 a2 a3];
 % Aw = z'
-w = z' \ A;
+% w is the vector containing the a, b, c parameters
+
+w = pinv(A) * z';
 
 a = w(1);
 b = w(2);
