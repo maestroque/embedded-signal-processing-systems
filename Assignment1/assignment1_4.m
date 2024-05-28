6e7633e201e3e17dc43556dac214f7fc6de9d83d
maestroque <kikasgeorg@ece.auth.gr>
2024-05-28 11:58:25 +0200

    Add assignment1_4 solution

--- Assignment1/assignment1_4.m
+++ Assignment1/assignment1_4.m
@@ -3,7 +3,14 @@ clear;
 % Load the variables ....
 load("data4.mat")
 
+ones = ones(length(x), 1);
+A = [x' y' ones];
+% Aw = z'
+w = z' \ A;
 
+a = w(1);
+b = w(2);
+c = w(3);
 
 % clear eveything except the required answers
 clearvars -EXCEPT a b c
