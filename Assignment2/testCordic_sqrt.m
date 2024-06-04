89c72874d4de756429026fa9c89c75c2f8bb4f4d
maestroque <kikasgeorg@ece.auth.gr>
2024-06-04 19:37:50 +0200

    Minor CORDIC test script

--- Assignment2/testCordic_sqrt.m
+++ Assignment2/testCordic_sqrt.m
@@ -2,8 +2,8 @@
 %
 % the following parameters define the fixed point representation of the
 % data variables in the computation of the square root
-DAT_BW = 20;   % the bit width of the data variables
-DAT_FL = 5;    % the fraction length of the data variables
+DAT_BW = 10;   % the bit width of the data variables
+DAT_FL = 8;    % the fraction length of the data variables
 DAT_S = true;  % the signedness of the data variables
 
 % the number of iterations of the CORDIC algorithm
@@ -13,7 +13,7 @@ NIter = 5;
 % default parameters for fixed point arithmetic
 F = fimath('OverflowAction','Saturate',...
     'ProductMode','SpecifyPrecision',...
-    'ProductWordLength', DAT_BW,...
+    'ProductWordLength', DAT_BW,...2
     'ProductFractionLength', DAT_FL,...
     'SumMode', 'SpecifyPrecision',...
     'SumWordLength', DAT_BW,...
@@ -42,6 +42,7 @@ end
 
 % compute the reference floating-point results with Matlab sqrt function
 x_ref = sqrt(v);   
+mse = mean(abs(x_ref - x_sqr_fixpt).^2);
 
 % Make a plot of the results
 figure;
@@ -57,4 +58,9 @@ absErr_fltpt = abs(x_ref - double(x_sqr));       % compute the error in the floa
 absErr_fixpt = abs(x_ref - double(x_sqr_fixpt)); % compute the error in the floating point CORDIC
 plot(v, absErr_fltpt, 'r-');
 plot(v, absErr_fixpt, 'g-');
+yline(mse, 'b--');
 title('Absolute Error (vs. MATLAB SQRT Reference Results)');
+
+fprintf("Mean Squared Error between CORDIC implementations (Floating vs Fixed Point):\n --> %g\n", mse)
+
+assert(mse < 3e-5, 'MSE is over the desired threshold')
